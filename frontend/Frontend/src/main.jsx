9ec55634e90f37ea6d58d7adb3f6e0211d4a6466
Dipshikha-Shinde <dipshikhashinde573@gmail.com>
2026-06-03 01:27:38 +0530

    Fix PublicProfile route

--- frontend/Frontend/src/main.jsx
+++ frontend/Frontend/src/main.jsx
@@ -13,7 +13,7 @@ import EditProfile from "./dashboard/EditProfile.jsx";
 import Requests from "./dashboard/Requests.jsx";
 import Chats from "./dashboard/Chats.jsx";
 import Sessions from "./dashboard/Sessions.jsx";
-import PublicProfile from "./pages/Publicprofile.jsx";
+import Publicprofile from "./pages/Publicprofile.jsx";
 import ProtectedRoute from "./components/ProtectRoute.jsx";
 import PublicOnlyRoute from "./components/publicOnlyRoute.jsx";
 import ForgotPassword from "./landing_page/Login/ForgotPassword.jsx";
