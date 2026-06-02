307867629358153b7ca3de6e96f221adbee45524
Dipshikha-Shinde <dipshikhashinde573@gmail.com>
2026-06-02 22:19:01 +0530

    Removed video calling feature and updated SkillSwap

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ import communityRoutes from "./routes/communityRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
 dotenv.config();
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
